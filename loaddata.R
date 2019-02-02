803b22fff3381b008269a6c30daea30d01b87143
Frank Jung <frankhjung@linux.com>
2019-02-02 15:28:54 +1100

    day 3

--- loaddata.R
+++ loaddata.R
@@ -2,9 +2,8 @@
 # Notes: 
 # * using littler package for script commandline execution
 
-# load required packages
-if (!require('lubridate')) { install.packages('lubridate') }
-if (!require('stringr')) { install.packages('stringr') }
+require('lubridate')
+require('stringr')
 
 # declare URL to dfset
 URL <- 'https://data.gov.au/dataset/7f90d314-fa64-4bae-8609-2e26ff48f6fa/resource/05390642-bdeb-4174-9bd5-c1df6e6d1e9e/download/2018-australian-sdg-indicator-9-2-2v2.csv'
